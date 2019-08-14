08a061bd85a21581487b382e80790c9f1ac9e6d4
Marc Hermon <hermonm@urbandaleschools.com>
2019-08-14 13:39:09 -0500

    Update 23.6_Jedi_Task_Histogram.py

--- 23.6_Jedi_Task_Histogram.py
+++ 23.6_Jedi_Task_Histogram.py
@@ -1,5 +1,22 @@
 '''
+----------------------
 Create your Histogram
+----------------------
+Hint: You will again need to import the numpy library and determine the mean and standard deviation
+of the data. You can do this like:
+
+average = np.mean(dataset)
+standev = np.std(dataset)
+
+You will create vertical lines at the mean, at +2 standard deviations above the mean 
+and -2 standard deviations below the mean. To create a vertical line research (axvline).
+
+Incorporate those values into the title. The Greek letters (mu) and (sigma) stand 
+for mean and standard deviation respectively. Research how to get those Greek letters
+into the title.
+
+Here is the data that needs to be used:
+ages = [22,55,62,45,21,22,34,42,42,4,99,102,110,120, 121,122,130,111,115,112,80,75,65,54,44,43,42,48]
 '''
 import matplotlib.pyplot as plt
 
