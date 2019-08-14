371a7ae9ccc9306fccba8e13bc19ab1cec1620d3
Marc Hermon <hermonm@urbandaleschools.com>
2019-08-14 13:36:26 -0500

    Update 23.5_Jedi_Task_Bar.py

--- 23.5_Jedi_Task_Bar.py
+++ 23.5_Jedi_Task_Bar.py
@@ -1,5 +1,15 @@
 '''
+----------------------
 Create your Bar Chart
+----------------------
+You will need to determine how to do a horizontal bar chart. 
+First make 13 red and white horizontal bars with a height of 1. 
+(width has now become height since we are using horizontal bars) 
+Use xticks and yticks to put 13 asterisks as the labels on the x and y axes. 
+Make a single blue horizontal bar. 
+Use Stars as the vertical label and Stripes as the horizontal label. 
+Change the title of your flag to include your name. 
+To get rid of whitespace on the top and right edges use plt.autoscale(tight='yes')
 '''
 import matplotlib.pyplot as plt
 
